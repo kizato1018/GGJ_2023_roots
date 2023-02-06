f44e9ab5e239726542e65fd34de82f4204e107b9
William <vvbest2012@gmail.com>
2023-02-06 10:51:19 +0800

    fixed ribbit

--- Assets/Scripts/Rabbit/Rabbit.cs
+++ Assets/Scripts/Rabbit/Rabbit.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Rabbit : MonoBehaviour, BattleAction
 {
     public int hp = 5;
-    public float Speed=1;
+    public float Speed = 1;
     public float sleep_time = 1.0f;
-    public float sleep_timer=0.0f;
+    public float sleep_timer = 0.0f;
     public float check_time = 3.0f;
-    public float check_timer=0.0f;
-    public RootData current_goal = null; 
+    public float check_timer = 0.0f;
+    public RootData current_goal = null;
     private Animator animator;
     // Start is called before the first frame update
     void Start()
@@ -23,21 +23,23 @@ public class Rabbit : MonoBehaviour, BattleAction
     {
         if (current_goal == null || check_timer > check_time)
         {
-            current_goal = RootsManager.instance.FindNearRoot(transform.position);
+            if (transform != null && RootsManager.instance != null)
+                current_goal = RootsManager.instance.FindNearRoot(transform.position);
             check_timer = 0.0f;
         }
         check_timer += Time.deltaTime;
-        if (current_goal != null) {
+        if (current_goal != null)
+        {
             Vector3 moveVector = (current_goal.worldPosition - transform.position);
             moveVector.Normalize();
             transform.Translate(moveVector * Speed * Time.deltaTime);
 
-            if (Vector3.Distance(transform.position,current_goal.worldPosition) < 0.01f)
+            if (Vector3.Distance(transform.position, current_goal.worldPosition) < 0.01f)
             {
                 animator.SetTrigger("attack");
                 AudioManager.instance.PlaySound("412068__inspectorj__chewing-carrot-a");
                 sleep_timer += Time.deltaTime;
-                if(sleep_timer > sleep_time)
+                if (sleep_timer > sleep_time)
                 {
                     RootsManager.instance.DeleteRoot(current_goal.worldPosition);
                     sleep_timer = 0.0f;
